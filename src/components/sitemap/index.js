9a9ca841e12797bd971b742f2c760cd1827c918f
hoanghuy-huy <huynhhoanghuy221122@gmail.com>
2024-11-19 22:22:29 +0700

    sitemap

---
+++ src/components/sitemap/index.js
@@ -0,0 +1,14 @@
+import { Sitemap } from 'react-sitemap';
+
+const sitemap = () => {
+  return (
+    <Sitemap>
+      <url>
+        <loc>https://harold.com.vn//</loc>
+        <lastmod>2024-01-01</lastmod>
+      </url>
+    </Sitemap>
+  );
+};
+
+export default sitemap;
